230efe830d68ffcd8ab4465cdde7f8193747de75
CrazyDave <adrieldavid81169@gmail.com>
2023-04-09 23:13:02 +0530

    collision detection on canvas

--- src/ball.js
+++ src/ball.js
@@ -4,7 +4,6 @@ export default class Ball {
         this.gameWidth = gameWidth;
         this.gameHeight = gameHeight;
 
-        this.speed = 5;
         this.size = 10;
 
         this.position = {
@@ -12,14 +11,31 @@ export default class Ball {
             y: 0,
         }
 
-        this.direction = {
-            x: 0,
-            y: 0,
+        this.speed = {
+            x: 0.2,
+            y: 0.2,
         }
     }
 
     draw(ctx) {
-        ctx.fillStyle = 'red'
-        ctx.fillRect(this.position.x, this.position.y, this.size, this.size)
+
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.position.x, this.position.y, this.size, this.size);
+
     }
+
+    update(deltaTime) {
+
+        //move ball around canvas
+        this.position.x += this.speed.x * deltaTime;
+        this.position.y += this.speed.y * deltaTime;
+
+        //collision on x-axis
+        if (this.position.x + this.size >= this.gameWidth || this.position.x <= 0) this.speed.x = -this.speed.x;
+
+        //collision on y-axis
+        if (this.position.y + this.size >= this.gameHeight || this.position.y <= 0) this.speed.y = -this.speed.y;
+
+    }
+       
 }
