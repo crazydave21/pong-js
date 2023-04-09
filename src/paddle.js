88f58ceab70d1c499e38ed452df912018f1617c6
CrazyDave <adrieldavid81169@gmail.com>
2023-04-09 23:14:30 +0530

    draw player 2 paddle on canvas

--- src/paddle.js
+++ src/paddle.js
@@ -4,14 +4,24 @@ export default class Paddle {
         this.width = 100;
         this.height = 15;
 
-        this.position = {
+        this.p1position = {
             x: gameWidth / 2 - this.width / 2,
             y: gameHeight - this.height - 10,
+        }
+
+        this.p2position = {
+            x: gameWidth / 2 - this.width / 2,
+            y: 10,
         }
     }
 
     draw(ctx) {
+        
         ctx.fillStyle = 'black';
-        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+        //draw player 1 paddle on canvas
+        ctx.fillRect(this.p1position.x, this.p1position.y, this.width, this.height);
+        //draw player 2 paddle on canvas
+        ctx.fillRect(this.p2position.x, this.p2position.y, this.width, this.height);
+
     }
 }
